c1e7b3f9b3388f4806d32d30b2bf1ea2888e20fe
andresaftari <andresaftari@gmail.com>
2020-10-12 18:45:21 +0700

    Added securities

--- core/src/main/java/com/andresaftari/core/di/CoreModule.kt
+++ core/src/main/java/com/andresaftari/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.andresaftari.core.data.source.remote.RemoteDataSource
 import com.andresaftari.core.data.source.remote.api.ApiService
 import com.andresaftari.core.domain.repo.IMealRepository
 import com.andresaftari.core.utils.CoreExecutor
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,18 +22,32 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MealDB>().mealDao() }
     single {
+        val keyPhrase: ByteArray = SQLiteDatabase.getBytes("andresaftari".toCharArray())
+        val factory = SupportFactory(keyPhrase)
         Room.databaseBuilder(
             androidContext(), MealDB::class.java, "foodiezz_db"
-        ).allowMainThreadQueries().fallbackToDestructiveMigration().build()
+        ).allowMainThreadQueries()
+            .openHelperFactory(factory)
+            .fallbackToDestructiveMigration()
+            .build()
+
     }
 }
 
 val networkModule = module {
     single {
+        val baseUrl = "www.themealdb.com"
+        val cerificatePinnerBuilder = CertificatePinner.Builder()
+            .add(baseUrl, "sha256/pz7CjjOO6yeiHWrcJ+RWljKC2pBYw+9O7XwRIl7HLn8=")
+            .add(baseUrl, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(baseUrl, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(cerificatePinnerBuilder)
             .build()
     }
 
